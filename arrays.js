aea49f025a65b5b108d46155d3cc21d522a4d85f
ashwikanjani1 <valasalaashwikanjani@gmail.com>
2025-01-20 12:29:10 +0530

    sending

--- arrays.js
+++ arrays.js
@@ -1,2 +1,19 @@
-let users =["ramya","sai nikhil","prabhas","rahul"];
-console.log(users[0]);
+// let users =["ramya","sai nikhil","prabhas","rahul"];
+// console.log(users[0]);
+let users = ["gita","dimple", "rahul", "prabhas"];
+
+// console.log(users[0]);
+// console.log(users[3]);
+// for(let i = 0; i< users.length; i++){
+//     console.log(users[i]);
+// }
+// users.map((user)=>{
+//     console.log(user);
+
+// })
+
+let x = users.forEach((user)=>{
+     //console.log(user);
+     return user;
+});
+console.log(x);
